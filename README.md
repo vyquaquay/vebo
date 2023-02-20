a85b30089d4fa6892d89bac0c5d3b93f7e9f32b9
vyquaquay <vydapgiai69@gmail.com>
2023-02-20 15:01:02 +0700

    Make CRUD

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # vebo
 web bong da mainleg
 100% mainleg ko he` co' ca' cuoc. Nhung neu ban quan tam thi` co' the? den' voi' 8xbet. 
-Nha` cai' hang` dau` chau au hien. nay dang la` nah` tai` tro. chinh' cho cau lac bo manchester city
+Nha` cai' hang` dau` chau au hien. nay dang la` nha` tai` tro. chinh' cho cau lac bo Manchester City
---
+++ src/Form/GameType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Game;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Match_ID')
+            ->add('Club_1')
+            ->add('Club1_score')
+            ->add('Club2_score')
+            ->add('Club_2')
+            ->add('Stadium')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Game::class,
+        ]);
+    }
+}
